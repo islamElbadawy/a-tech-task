d3ba74eaee5b84412856fdba34f8382b74e0364c
islamElbadawy <islam.201211@gmail.com>
2024-01-15 22:28:44 +0200

    initialize project files and libs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './components/card/card.component';
+import { TableComponent } from './components/table/table.component';
+import { ChartComponent } from './components/chart/chart.component';
+import { NgApexchartsModule } from 'ng-apexcharts';
+import { QRCodeModule } from 'angularx-qrcode';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, CardComponent, TableComponent, ChartComponent],
+  imports: [BrowserModule, AppRoutingModule, NgApexchartsModule, QRCodeModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
