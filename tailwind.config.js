7b472309bfbdbece5cf3d860dfbd5bde0f24af65
islamElbadawy <islam.201211@gmail.com>
2024-01-15 22:53:35 +0200

    adding fonts to project

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {},
+    fontFamily: {
+      english: ["Inter", "sans-serif"],
+      arabic: ["Neo Sans Arabic", "sans-serif"],
+    },
   },
   plugins: [],
 };
