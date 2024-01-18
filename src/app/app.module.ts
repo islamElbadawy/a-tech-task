a7d5248c50d8712143710d60ac5c9de0e80d7405
islamElbadawy <islam.201211@gmail.com>
2024-01-18 16:27:21 +0200

    copy text is done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,17 +10,22 @@ import { ChartComponent } from './components/chart/chart.component';
 import { QRCodeModule } from 'angularx-qrcode';
 import { EchartsxModule } from 'echarts-for-angular';
 import { TooltipModule } from 'primeng/tooltip';
+import { ClipboardModule } from 'ngx-clipboard';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
 
 @NgModule({
   declarations: [AppComponent, CardComponent, TableComponent, ChartComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     EchartsxModule,
     QRCodeModule,
     TooltipModule,
+    ToastModule,
   ],
-  providers: [],
+  providers: [MessageService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrl: './card.component.css'
+  styleUrl: './card.component.css',
 })
 export class CardComponent {
+  qrCodeValue = 'www.tazkty.com/473847';
 
+  constructor(private messageService: MessageService) {}
+
+  copyText(): void {
+    navigator.clipboard
+      .writeText(this.qrCodeValue)
+      .then(() => {
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Ticket',
+          detail: 'Text copied',
+          sticky: false,
+          life: 2000,
+        });
+      })
+      .catch(() => {
+        console.error('Unable to copy text');
+      });
+  }
 }
