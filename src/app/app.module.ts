573634626c0f84b2b23aaa98f8fe27227b0f7256
islamElbadawy <islam.201211@gmail.com>
2024-01-20 07:32:00 +0200

    Initiate table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CardComponent } from './components/card/card.component';
-import { TableComponent } from './components/table/table.component';
-import { ChartComponent } from './components/chart/chart.component';
-import { QRCodeModule } from 'angularx-qrcode';
-import { EchartsxModule } from 'echarts-for-angular';
-import { TooltipModule } from 'primeng/tooltip';
-import { ToastModule } from 'primeng/toast';
-import { MessageService } from 'primeng/api';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { CardComponent } from './components/card/card.component'
+import { TableComponent } from './components/table/table.component'
+import { ChartComponent } from './components/chart/chart.component'
+import { QRCodeModule } from 'angularx-qrcode'
+import { EchartsxModule } from 'echarts-for-angular'
+import { TooltipModule } from 'primeng/tooltip'
+import { ToastModule } from 'primeng/toast'
+import { MessageService } from 'primeng/api'
+import { TableModule } from 'primeng/table'
+import { MultiSelectModule } from 'primeng/multiselect'
+import { DropdownModule } from 'primeng/dropdown'
+import { TagModule } from 'primeng/tag'
+import { SliderModule } from 'primeng/slider'
+import { ProgressBarModule } from 'primeng/progressbar'
 
 @NgModule({
   declarations: [AppComponent, CardComponent, TableComponent, ChartComponent],
@@ -23,6 +29,12 @@ import { MessageService } from 'primeng/api';
     QRCodeModule,
     TooltipModule,
     ToastModule,
+    TableModule,
+    MultiSelectModule,
+    DropdownModule,
+    TagModule,
+    SliderModule,
+    ProgressBarModule,
   ],
   providers: [MessageService],
   bootstrap: [AppComponent],
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
+  content: ['./src/**/*.{html,ts}'],
   theme: {
     extend: {
       container: {
         screens: {
-          sm: "640px",
-          md: "768px",
-          lg: "1024px",
-          xl: "1280px", // Change the maxWidth for xl to 1280px
+          sm: '640px',
+          md: '768px',
+          lg: '1024px',
+          xl: '1280px', // Change the maxWidth for xl to 1280px
         },
       },
     },
     fontFamily: {
-      english: ["Inter", "sans-serif"],
-      arabic: ["Neo Sans Arabic", "sans-serif"],
-      mont: ["Montserrat", "sans-serif"],
+      english: ['Inter', 'sans-serif'],
+      arabic: ['Neo Sans Arabic', 'sans-serif'],
+      mont: ['Montserrat', 'sans-serif'],
+      poppins: ['poppins', 'sans-serif'],
     },
     colors: {
-      bgColor: "#F3F5F7",
-      mainColor: "#8A74F9",
-      textColor: "#1C1C28",
-      textLight: "#99A1B7",
-      tooltipColorBg: "#1E1B39",
+      bgColor: '#F3F5F7',
+      mainColor: '#8A74F9',
+      textColor: '#1C1C28',
+      textLight: '#99A1B7',
+      tooltipColorBg: '#1E1B39',
     },
   },
   plugins: [],
-};
+}
